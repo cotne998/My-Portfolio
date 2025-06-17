690e91a5eef54dff37f16c5a21346918c732a031
cotne998 <cotnecincadze998@gmail.com>
2025-06-18 00:09:30 +0400

    removed extra useRef

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 interface Nav {
